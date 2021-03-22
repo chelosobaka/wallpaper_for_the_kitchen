c66d34392d17753e19f0780cfc2c79eb5c80c907
chelosobaka <chelosobaka93@mail.ru>
2021-03-22 03:11:08 +0300

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users, path: 'order'
 
   get '/users/:id', to: 'users#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
